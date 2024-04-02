1ee5ad30fa81ea50fee6baf4695e13cc4cb98c0b
Guilherme Arcencio <guiarcencio@hotmail.com>
2024-04-01 21:31:15 -0300

    changes N in run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ makedirs("results/multicore", exist_ok=True)
 
 run(["make", "multicore"])
 
-for N in [100000, 150000, 200000]:
+for N in [25000, 50000, 100000]:
     for n_threads in [1, 2, 4, 8, 16, 32]:
         runtimes = []
         for _ in range(5):
